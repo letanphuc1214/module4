f5a4b0cfd84ad14fa012b9527fcc264489ea2745
letanphuc1214 <tanphuc1214@gmail.com>
2020-11-05 10:26:33 +0700

    update

---
+++ week2/TH-QuanLyKhachHang-PhanTrangVaTimKiem/src/main/java/com/codegym/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.codegym.repository;
+
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+    //    Iterable<Customer> findAllByProvince(Province province);
+//
+//    Customer findOne(Long id);
+//
+//    void delete(Long id);
+    Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
+}
